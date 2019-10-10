dce0a3efc1e7fcb9ae978d6afa393d16b10846cd
mokunshao <mokunshao@foxmail.com>
2019-10-11 03:33:56 +0800

    Create check.php

---
+++ e/check.php
@@ -0,0 +1,35 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  $sql = 'SELECT * FROM user WHERE `username` = :username';
+
+  $stmt = $pdo->prepare($sql);
+
+  $stmt->execute([
+    'username' => $username,
+  ]);
+  if (empty($username)) {
+    die('请输入用户名');
+  }
+  if (empty($password)) {
+    die('请输入密码');
+  }
+  $user = $stmt->fetch(PDO::FETCH_ASSOC);
+  if ($user === false) {
+    die('不存在该用户名');
+  }
+  if ($user['password'] === md5($password)) {
+    echo '登录成功';
+    $_SESSION['name'] = $user['name'];
+  } else {
+    die('密码错误');
+  }
+} else {
+  die('请求类型错误');
+}
+echo '<br>';
+echo '<a href=\'/e/dispatch.php?action=index\'>首页</a>';
