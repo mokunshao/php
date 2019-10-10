d6c3219facaad538f9d99c41b481364c565afe49
mokunshao <mokunshao@foxmail.com>
2019-10-11 03:34:02 +0800

    Create dispatch.php

---
+++ e/dispatch.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+require_once __DIR__ . '/connect.php';
+
+$action =  isset($_GET['action']) ? $_GET['action'] : 'index';
+$action = htmlentities(strtolower(trim($action)));
+
+switch ($action) {
+  case 'login':
+    include __DIR__ . '/login.php';
+    break;
+  case 'check':
+    include __DIR__ . '/check.php';
+    break;
+  case 'logout':
+    include __DIR__ . '/logout.php';
+    break;
+  case 'index':
+    include __DIR__ . '/index.php';
+    break;
+  default:
+    include __DIR__ . '/index.php';
+    break;
+}
