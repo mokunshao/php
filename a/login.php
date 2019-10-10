1eadf1eef698a0d3797609b900166b857b9065d4
mokunshao <mokunshao@foxmail.com>
2019-10-10 23:07:00 +0800

    Create login.php

---
+++ a/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>登录</title>
+</head>
+
+<body>
+  <h3>登录</h3>
+  <form action="./check.php" method="post">
+    <label for="username">用户名</label>
+    <input type="text" name="username" id="username">
+    <br>
+    <label for="password">密码</label>
+    <input type="password" name="password" id="password">
+    <br>
+    <button type="submit">登录</button>
+  </form>
+</body>
+
+</html>
