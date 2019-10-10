85a1e745bd9317cd66072e930ea4229212fef971
mokunshao <mokunshao@foxmail.com>
2019-10-10 15:59:17 +0800

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,17 @@
+<?php
+$db = [
+  'type' => 'mysql',
+  'host' => 'localhost:8889',
+  'dbname' => 'haha',
+  'username' => 'root',
+  'password' => 'root',
+];
+$dsn = "{$db['type']}:host={$db['host']};dbname={$db['dbname']}";
+try {
+  $pdo = new PDO($dsn, $db['username'], $db['password']);
+} catch (PDOException $e) {
+  die($e->getMessage());
+  // 相当于：
+  // echo $e->getMessage();
+  // exit;
+}
