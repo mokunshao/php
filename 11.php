075db806a6e3bff26f8f8ef6d059fe85b48f74e2
mokunshao <mokunshao@foxmail.com>
2019-10-11 22:46:25 +0800

    Update 11.php

--- 11.php
+++ 11.php
@@ -33,3 +33,17 @@ $obj2 = new Demo2(123);
 print_r($obj2);
 echo Demo2::getPrice();
 echo Demo2::$price;
+
+class Demo3
+{
+  const ABC = 123;
+  static $DEF = 456;
+  static function getABC()
+  {
+    return [self::ABC, self::$DEF];
+  }
+}
+
+echo Demo3::ABC;
+echo Demo3::$DEF;
+print_r(Demo3::getABC());
