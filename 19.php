c39a4c5fc3cc53f245d6307eea5a4b794cd53281
mokunshao <mokunshao@foxmail.com>
2019-10-17 17:19:17 +0800

    Create 19.php

---
+++ 19.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace _19;
+
+echo __NAMESPACE__, '<br>';
+
+class Dog
+{ }
+echo Dog::class, '<br>';
+
+namespace _19\one;
+
+echo __NAMESPACE__, '<br>';
+
+class Dog
+{ }
+echo Dog::class, '<br>';
+
+namespace _19\one\two;
+
+echo __NAMESPACE__, '<br>';
+
+class Dog
+{ }
+echo Dog::class, '<br>';
